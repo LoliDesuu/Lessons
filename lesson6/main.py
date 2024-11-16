be9ecce31976d63fb2ba6fdb1c1d5a8fe9ba0a12
LoliDesuu <84105402+LoliDesuu@users.noreply.github.com>
2024-11-16 21:07:29 +0300

    туц

--- lesson6/main.py
+++ lesson6/main.py
@@ -1,10 +1,21 @@
 import random
 
-number = random.randint(3, 21)
+number = random.randint(3, 20)
 print(number)
 
+# def get_unique_numbers(text):
+#     unique = ''
+#     for number in text:
+#         if number in unique:
+#             continue
+#         else:
+#             unique += number
+#         return unique
+
+text = ''
 for x in range(1, number):
     for y in range(1, number):
         if number % (x + y) == 0:
-            text = {f"{x}{y}"}
-    print(text)
+            text += f"{x}{y}"
+
+print(text)
---
+++ lesson7/main.py
@@ -0,0 +1,38 @@
+# Функция count_calls подсчитывающая вызовы остальных функций.
+
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+# Функция string_info принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре,
+# строку в нижнем регистре.
+
+def string_info(string):
+    global calls
+    count_calls()
+    len_string = len(string)
+    my_tuple = (len_string, string.upper(), string.lower())
+    return my_tuple
+
+# Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке,
+# False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
+
+def is_contains(string, list):
+    global calls
+    count_calls()
+    for elem in list:
+        string = string.lower()
+        elem = elem.lower()
+        is_equals = False
+        if string == elem:
+            is_equals = True
+    return is_equals
+
+# Проверки:
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
