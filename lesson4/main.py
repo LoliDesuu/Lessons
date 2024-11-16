5f4229bb4462737c295455d265fdc90852b0dd88
LoliDesuu <84105402+LoliDesuu@users.noreply.github.com>
2024-11-16 18:50:03 +0300

    туц

--- lesson4/main.py
+++ lesson4/main.py
@@ -1,2 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
+primes = []
+not_primes = []
+numbers.remove(1)
+
+for elem in numbers:
+    is_prime = True
+    for divinity in range(2, elem):
+        if elem % divinity == 0:
+            is_prime = False
+            not_primes.append(elem)
+            break
+
+    if (is_prime != False):
+        primes.append(elem)
+
+print(primes)
+print(not_primes)
 
