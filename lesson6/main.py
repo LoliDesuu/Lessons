495b9a851ba5fb85fc4e2da429a37e2d22328234
LoliDesuu <84105402+LoliDesuu@users.noreply.github.com>
2024-11-16 19:56:58 +0300

    Update main.py

--- lesson6/main.py
+++ lesson6/main.py
@@ -1,5 +1,10 @@
 import random
 
-number = random.randint(3, 20)
+number = random.randint(3, 21)
 print(number)
 
+for x in range(1, number):
+    for y in range(1, number):
+        if number % (x + y) == 0:
+            text = {f"{x}{y}"}
+    print(text)
