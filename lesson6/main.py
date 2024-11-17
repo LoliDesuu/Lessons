7e9ab865e037ae62d9b15c111952fe319e48d482
LoliDesuu <84105402+LoliDesuu@users.noreply.github.com>
2024-11-17 19:19:35 +0300

    Update main.py

--- lesson6/main.py
+++ lesson6/main.py
@@ -6,7 +6,7 @@ print(f"Число на первом камне: {number}")
 
 text = ''
 for x in range(1, number):
-    for y in range(1, number):
+    for y in range(x+1, number):
         if number % (x + y) == 0:
             text += f"{x}{y}"
 
