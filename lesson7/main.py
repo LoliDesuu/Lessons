c4ac705e3d3f80e8651b018d789bcb5471d3bf8f
LoliDesuu <84105402+LoliDesuu@users.noreply.github.com>
2024-11-17 20:41:59 +0300

    Update main.py

--- lesson7/main.py
+++ lesson7/main.py
@@ -1,10 +1,13 @@
 # Функция count_calls подсчитывающая вызовы остальных функций.
 
 calls = 0
+
+
 def count_calls():
     global calls
     calls += 1
     return calls
+
 
 # Функция string_info принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре,
 # строку в нижнем регистре.
@@ -15,6 +18,7 @@ def string_info(string):
     len_string = len(string)
     my_tuple = (len_string, string.upper(), string.lower())
     return my_tuple
+
 
 # Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке,
 # False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
@@ -30,9 +34,10 @@ def is_contains(string, list):
             is_equals = True
     return is_equals
 
+
 # Проверки:
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 print(calls)
