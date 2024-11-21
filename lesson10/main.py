dcc2efc76f9a75b47168f7df227ab89329d10af1
LoliDesuu <84105402+LoliDesuu@users.noreply.github.com>
2024-11-21 21:56:46 +0300

    Create main.py

---
+++ lesson10/main.py
@@ -0,0 +1,13 @@
+def single_root_words(root_word: str, *other_words):
+    same_words = []
+    for strings in other_words:
+        if strings.lower().count(root_word.lower()) == True or root_word.lower().count(strings.lower()) == True:
+            same_words.append(strings)
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)
+print(result2)
