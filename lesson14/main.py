cf3b16179606c3ed0356345e6be8775ab5a07233
LoliDesuu <84105402+LoliDesuu@users.noreply.github.com>
2024-12-10 13:46:46 +0300

    Update main.py

--- lesson14/main.py
+++ lesson14/main.py
@@ -3,4 +3,5 @@ def test_function():
         print("Я в области видимости функции test_function")
     inner_function()
 
-inner_function()
+test_function()
+# inner_function() - не вызывается, так как функция находится в объемляющей области видимости test_function()
